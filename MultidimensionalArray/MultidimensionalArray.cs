4b400cca76969248a3d70885a0a64a9751c7e17f
L8e9603 <L8e9603@gmail.com>
2023-03-19 16:29:21 +0900

    2023.03.19

---
+++ MultidimensionalArray/MultidimensionalArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int size;
+
+		Console.Write("Enter Size : ");
+
+		size = int.Parse(Console.ReadLine());
+
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size - i; j++)
+			{
+				Console.Write("*");
+			}
+
+			Console.WriteLine();
+		}
+	}
+}
