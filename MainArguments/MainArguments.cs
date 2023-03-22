36f92fe2eeb245c2f111a691ba602ca8aeeb3e84
L8e9603 <L8e9603@gmail.com>
2023-03-22 17:00:59 +0900

    2023.03.22

---
+++ MainArguments/MainArguments.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MainArguments
+{
+    public class MainArguments
+    {
+        public static void Main(string[] args)
+        {
+            for (int i=0; i<args.Length; ++i)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
