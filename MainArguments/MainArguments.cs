05a21eacae095fdb4dc83a1f0eb0b22bfc2c3a0a
L8e9603 <L8e9603@gmail.com>
2023-03-27 21:36:10 +0900

    2023.03.27

--- MainArguments/MainArguments.cs
+++ MainArguments/MainArguments.cs
@@ -9,6 +9,7 @@ namespace MainArguments
             for (int i=0; i<args.Length; ++i)
             {
                 Console.WriteLine($"args[{i}] = {args[i]}");
+                Console.WriteLine($"args[{i}] = {args[i]}");
             }
         }
     }
