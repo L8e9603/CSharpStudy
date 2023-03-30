1accbd221fdddbb42174fe54112dd45d7839617a
L8e9603 <L8e9603@gmail.com>
2023-03-30 21:26:50 +0900

    2023.03.30

--- MainArguments/MainArguments.cs
+++ MainArguments/MainArguments.cs
@@ -10,6 +10,19 @@ namespace MainArguments
             {
                 Console.WriteLine($"args[{i}] = {args[i]}");
             }
+
+            Divide(0f, 1f);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="numerator"></param>
+        /// <param name="denominator"></param>
+        /// <returns></returns>
+        static float Divide(float numerator, float denominator)
+        {
+            return numerator / denominator;
         }
     }
 }
