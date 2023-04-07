c9e85086f38615d3a0fadd4e5b6747a869df4dc4
L8e9603 <L8e9603@gmail.com>
2023-04-07 19:31:26 +0900

    2023.04.07

---
+++ 230407RecursiveFactorial/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    class Program
+    {
+        #region BUILT IN METHOD
+        static void Main(string[] args)
+        {
+            const ulong FACTORIAL = 10;
+            
+            Console.Write("NoneRecursiveFactorial : ");
+            Console.WriteLine(NoneRecursiveFactorial(FACTORIAL));
+
+            Console.Write("RecursiveFactorial : ");
+            Console.WriteLine(RecursiveFactorial(FACTORIAL));
+
+        }
+        #endregion
+
+        #region CUSTOM METHOD
+        /// <summary>
+        /// 반복문을 이용한 팩토리얼
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        static ulong NoneRecursiveFactorial(ulong n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            uint factorial = 1;
+            
+            for (uint i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+
+        /// <summary>
+        /// 재귀 함수를 이용한 팩토리얼
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        static ulong RecursiveFactorial(ulong n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * RecursiveFactorial(n-1); // 5! == 5 * 4! == 5 * 4 * 3! ==  5 * 4 * 3 * 2! == 5 * 4 * 3 * 2 * 1
+        }
+        #endregion
+    }
+}
