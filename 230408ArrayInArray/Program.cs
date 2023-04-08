c093eb6105a031e0ca27813e7b7a50f8f10007e4
L8e9603 <L8e9603@gmail.com>
2023-04-08 23:39:18 +0900

    2023.04.08

---
+++ 230408ArrayInArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArrayInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[3];  // int 자료형인 원소를 가지고, 이름이 number인 배열 선언
+                                        // 그 배열은 공간이 3개
+
+            string[][] classrooms = new string[3][]; /// string[][] classrooms -> string 배열을 원소로 가지는 배열 classrooms 선언
+                                                     /// new string[3][] -> classrooms 배열은 공간이 3개인 배열이다
+                                                     /// 
+            int classIndex = 0; // 0번째 == 1반
+            string[] studentNames = classrooms[classIndex]; // 1반에 접근
+        }
+    }
+}
--- 230408RandomShuffling/Program.cs
+++ 230408RandomShuffling/Program.cs
@@ -7,23 +7,23 @@ namespace RandomShuffling
     {
         static void Main(string[] args)
         {
-            const int SEED = 0;
+            const int SEED = 0; // 없어도 됨
             int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
 
             Console.Write("Before Shuffling : ");
             Console.WriteLine($"[{string.Join(", ", numbers)}]");
 
-            Random random = new Random(SEED); // 정해진 시드값으로 Random 클래스형 변수 생성
+            Random random = new Random(SEED);
 
-            for (int i = numbers.Length - 1; i > 0; i--) // number 배열의 길이 -1 만큼 반복
+            for (int i = numbers.Length - 1; i > 0; i--) // 뒤에서부터 반복하는 반복문
             {
-                int j = random.Next(0, i); // i는 numbers 배열의 인덱스를 의미하고, 반복할 때마다 1씩 줄어듦, j에는 섞을 숫자의 배열 인덱스 저장
+                int j = random.Next(0, i);
                 int temp = numbers[j];
                 numbers[j] = numbers[i];
                 numbers[i] = temp;
             }
 
-            Console.Write("After Shuffling : ");
+            Console.Write("After Shuffling :  ");
             Console.WriteLine($"[{string.Join(", ", numbers)}]");
         }
     }
