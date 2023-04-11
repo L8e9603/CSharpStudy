44a3b56366bafa3e348760f01d8fb39ec2b4869e
L8e9603 <L8e9603@gmail.com>
2023-04-11 22:38:02 +0900

    2023.04.11

--- 230410ArrayOfArrays/Program.cs
+++ 230410ArrayOfArrays/Program.cs
@@ -10,8 +10,58 @@ namespace ArrayOfArrays
             const int MONTHS_IN_A_YEAR = 12; // 상수 선언해주면 코드 읽기 편하다
             int[] daysInEachMonth = new int[MONTHS_IN_A_YEAR] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-            string[][] calendar = new string[MONTHS_IN_A_YEAR][]; // 바깥배열 12개 공간 만들기
+            string[][] calendar = new string[MONTHS_IN_A_YEAR][]; // string배열을 원소로 가지는 배열 calendar에 12개 공간 만들기
 
+            for (int i = 0; i < MONTHS_IN_A_YEAR; i++)
+            {
+                calendar[i] = new string[daysInEachMonth[i]]; // 바깥 배열의 각 요소(1~12월)를 문자열 배열로 초기화, 문자열 배열의 길이는 제각각임
+            }
+
+            while (true) // 무한 루프 시작
+            {
+                // month 입력 받기
+                Console.Write("Enter the Month (1 - 12) : "); // month 물어보기, 1 - 12 사이의 수여야 함
+                string monthString = Console.ReadLine(); // 사용자에게 입력받은 값을 monthString 문자열 변수에 대입
+                int month = int.Parse(monthString); // 입력받은 문자열을 int로 파싱 후 정수형 변수 month에 대입
+
+                if (0 >= month || month > 12) // month가 1 - 12 사이의 값이 아니면 while문 종료
+                {
+                    Console.WriteLine("Invalid range of month. Terminating program.");
+                    break;
+                }
+
+
+                // day 입력 받기
+                Console.Write($"Enter the Day (1 - {calendar[month - 1].Length}) : "); // 입력받은 month의 값에 따라 Day의 범위 출력 해줌
+                string dayString = Console.ReadLine();
+                int day = int.Parse(dayString);
+
+                if (0 >= day || day > calendar[month - 1].Length) // day가 month에 따른 범위가 아니면 while문 종료
+                {
+                    Console.WriteLine("Invalid range of day. Terminating program.");
+                    break;
+                }
+
+
+                // 선택한 month와 day에 스케줄 입력
+                Console.Write("Enter your schedule : ");
+                string schedule = Console.ReadLine();
+                calendar[month - 1][day - 1] = schedule;
+
+
+                // 등록된 모든 스케줄 출력
+                Console.WriteLine("---------------------------------------------------------");
+                for (int i = 0; i < MONTHS_IN_A_YEAR; i++) // month 순회
+                {
+                    for (int j = 0; j < calendar[i].Length; j++) // month에 해당하는 day의 길이만큼 순회
+                    {
+                        if (!string.IsNullOrEmpty(calendar[i][j])) // 비어있지 않은 요소 출력
+                        {
+                            Console.WriteLine($"{i + 1} / {j + 1} : {calendar[i][j]}\n");
+                        }
+                    }
+                }
+            }
         }
     }
 }
---
+++ 230411ParseTextMessage/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace ParseTextMessage
+{
+    class Program
+    {
+        #region BUILT-IN METHOD
+        static void Main(string[] args)
+        {
+            #region 문자열 토큰화 메서드 구현 연습, 문자 메세지 해석하기 기능 구현 전 연습
+            string nameText = "Karen,Mary,Christina,Betty,Pope,Sun";
+
+            foreach (var token in StringToTokens(nameText))
+            {
+                Console.WriteLine(token);
+            }
+
+            // Console.WriteLine(String.Join(Environment.NewLine, StringToTokens(nameText))); // 위 foreach문과 동일한 코드
+
+            // Console.WriteLine(StringToTokens(nameText)); //배열을 출력하려한 코드지만 작동하지 않음, System.string[] 출력됨
+            #endregion
+
+            #region 문자 메세지 해석하기
+
+            #endregion
+        }
+        #endregion
+
+        #region CUSTOM METHOD
+        /// <summary>
+        /// 문자열을 토큰화 시키는 메서드
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <returns>string 배열 리턴, 배열의 각 요소는 토큰</returns>
+        static string[] StringToTokens(string plainText)
+        {
+            string[] tokens;
+            string tempString = plainText;
+            int commaCount = 0;
+
+            // 콤마 개수를 알아내 토큰의 개수 파악
+            while (tempString.IndexOf(',') != -1)
+            {
+                commaCount++;
+                tempString = tempString.Substring(tempString.IndexOf(',') + 1);
+            }
+
+            tokens = new string[commaCount + 1]; // commaCount + 1 == 토큰의 개수, 토큰 개수만큼 배열 공간 잡기
+            
+            tempString = plainText; // for문에서 돌리기 위해 다시 초기화
+
+            // 토큰 배열에 토큰 대입
+            for (int i = 0; i < commaCount + 1; i++)
+            {
+                if (tempString.IndexOf(',') != -1) // 문자열에 콤마가 있다면
+                {
+                    tokens[i] = tempString.Substring(0, tempString.IndexOf(',')); // 토큰 배열에 콤마 전까지 잘라넣고
+                    tempString = tempString.Substring(tempString.IndexOf(',') + 1); // 콤마 이후 문자열 잘라내 대입
+                }
+                else // 마지막 토큰 도달
+                {
+                    tokens[i] = tempString;
+                }
+            }
+
+            return tokens;
+        }
+    }
+    #endregion
+}
