51048c527280f96cac9496bda35485ed7bf1fab7
L8e9603 <L8e9603@gmail.com>
2023-04-17 09:33:52 +0900

    2023.04.17

---
+++ 230417FunctionOverloading/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FunctionOverloading
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+        
+        static void Print(int score)
+        {
+            Console.WriteLine(score);
+        }
+
+        static void Print(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
