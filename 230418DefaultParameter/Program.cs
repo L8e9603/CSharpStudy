4cdcc1a80bd76d8d88fb18ece917b7754e0ade8c
L8e9603 <L8e9603@gmail.com>
2023-04-19 01:19:52 +0900

    2023.04.19

---
+++ 230418DefaultParameter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DefaultParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bar("POCU");
+            Bar("POCU", "COMP1500");
+            Bar("POCU", "COMP1500", "Programming");
+        }
+        
+        static void Bar(string s, string s2 = "", string s3 = "")
+        {
+             Console.WriteLine($"{s}, {s2}, {s3}");
+        }
+    }
+}
