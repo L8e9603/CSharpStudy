e8396c5bb3f80671121fa4c63ec9b64c73362614
L8e9603 <L8e9603@gmail.com>
2023-04-26 21:36:09 +0900

    2023.04.26

---
+++ 230425StaticMethod/CourseCode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StaticMethod
+{
+    public class CourseCode
+    {
+        #region 생성자
+        public CourseCode(ESubject subject, ushort number)
+        {
+            Subject = subject;
+            Number = number;
+        }
+        #endregion
+
+        #region 멤버 변수, 프로퍼티
+        public ESubject Subject { get; private set; }
+
+        public ushort Number { get; private set; }
+        #endregion
+
+        #region 멤버 함수
+        /// <summary>
+        /// 들어온 문자열을 과목과 번호로 나누고, 생성자 호출시켜 개체 생성 후 반환시키는 메서드
+        /// </summary>
+        /// <param name="courseCodeString"></param>
+        /// <returns></returns>
+        public static CourseCode Parse(string courseCodeString)
+        {
+            string subjectString = courseCodeString.Substring(0, 4);
+            ESubject subject = Enum.Parse<ESubject>(subjectString);
+
+            string numberString = courseCodeString.Substring(4);
+            ushort number = ushort.Parse(numberString);
+
+            return new CourseCode(subject, number); // 정적 멤버 함수로 생성자 호출시켜 개체 생성 후 반환
+        }
+        #endregion
+    }
+}
--- 230425StaticMethod/ESubject.cs
+++ 230425StaticMethod/ESubject.cs
@@ -1,6 +1,9 @@
 ﻿namespace StaticMethod
 {
+    // 네임스페이스에는 열거형, 구조체 등 선언이 가능하다
     public enum ESubject
     {
+        COMP,
+        MATH
     }
 }
